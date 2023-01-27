b9f2f716323a0b788dbeb54c8ce00d3040386a3e
Paulus Christofel S <75057796+pythpal20@users.noreply.github.com>
2023-01-27 15:49:09 +0700

    update connection php
    set all to localhost settings

--- config/connection.php
+++ config/connection.php
@@ -1,17 +1,17 @@
 <?php
 	// koneksi 1 mysqli object
 	$DB_HOST 	= 'localhost';
-	$DB_USER 	= 'horek940_salsys';
-	$DB_PASS	= 'Garuda752021';
+	$DB_USER 	= 'root';
+	$DB_PASS	= '';
 	$DATABASE 	= 'horek940_salsys';
 	$db1 		= new mysqli($DB_HOST, $DB_USER, $DB_PASS, $DATABASE);
 	// koneksi 2 mysqli
-	$connect = mysqli_connect("localhost", "horek940_salsys", "Garuda752021", "horek940_salsys");
+	$connect = mysqli_connect("localhost", "root", "", "horek940_salsys");
 	
 	// koneksi 3 PDO
 	$host = 'localhost';
-	$username = 'horek940_salsys';
-	$password = 'Garuda752021'; 
+	$username = 'root';
+	$password = ''; 
 	$database = 'horek940_salsys'; 
 	$pdo = new PDO('mysql:host='.$host.';dbname='.$database, $username, $password);
 	
@@ -19,8 +19,8 @@
 
 	// koneksi agil keepstock mysqli object
 	$DB_HOST_AGIL 	= 'localhost';
-	$DB_USER_AGIL 	= 'horek940_ngudang';
-	$DB_PASS_AGIL	= 'garuda75';
+	$DB_USER_AGIL 	= 'root';
+	$DB_PASS_AGIL	= '';
 	$DATABASE_AGIL 	= 'horek940_ngudang';
 	$DB_AGIL 		= new mysqli($DB_HOST_AGIL, $DB_USER_AGIL, $DB_PASS_AGIL, $DATABASE_AGIL);
 
